8320d958c298bd5c0b3fd2aaab2f8042db299ed4
wlbf <lbf1353@live.com>
2018-11-03 00:17:31 +0800

    simplify

--- ThreadPool.h
+++ ThreadPool.h
@@ -14,9 +14,7 @@
 class ThreadPool {
 public:
     ThreadPool(size_t);
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::result_of<F(Args...)>::type>;
+    void enqueue(std::function<void()>&& f);
     size_t queue_size() { return task_queue_size; }
     ~ThreadPool();
 private:
@@ -46,8 +44,9 @@ inline ThreadPool::ThreadPool(size_t threads)
 
                     {
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
+                        while ( !this->stop && this->tasks.empty()) {
+                            this->condition.wait(lock);
+                        }
                         if(this->stop && this->tasks.empty())
                             return;
                         task = std::move(this->tasks.front());
@@ -62,17 +61,8 @@ inline ThreadPool::ThreadPool(size_t threads)
 }
 
 // add new work item to the pool
-template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
+void ThreadPool::enqueue(std::function<void()>&& f)
 {
-    using return_type = typename std::result_of<F(Args...)>::type;
-
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        
-    std::future<return_type> res = task->get_future();
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
 
@@ -80,11 +70,10 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
         if(stop)
             throw std::runtime_error("enqueue on stopped ThreadPool");
 
-        tasks.emplace([task](){ (*task)(); });
+        tasks.emplace(f);
         task_queue_size++;
     }
     condition.notify_one();
-    return res;
 }
 
 // the destructor joins all threads
